0ae8fc3fbe972d57645133d0cdb2cb8c19a8e3aa
PedroPiuma <lpap1000@gmail.com>
2022-04-08 20:38:51 -0300

    fix menu hamburguer

--- src/scripts/hambuguer.js
+++ src/scripts/hambuguer.js
@@ -2,20 +2,26 @@ window.onload = () => {
     const hamburguer = document.querySelector('.burguer-button')
     const navbar = document.querySelector('.navbar-links')
     const link = document.querySelectorAll('.navbar-link')
-    hamburguer.addEventListener('click', event => {
-        // hamburguer.remove('svg')
-        link[0].style.paddingBottom = '40px'
-        link[1].style.paddingBottom = '40px'
-        navbar.style.display = 'flex'
-        navbar.style['flex-direction'] = 'column'
-        navbar.style.position = 'fixed'
-        navbar.style.top = '57px'
-        navbar.style.left = 0
-        navbar.style.bottom = 0
-        navbar.style.width = '100vh'
-        navbar.style.backgroundColor = '#171718'
-        navbar.style.paddingLeft = '16px'
-        navbar.style.paddingTop = '28.5px'
-        document.documentElement.style.overflow = 'hidden'
-    })
+    const menu = document.querySelector('#menu')
+    const close = document.querySelector('#close')
+    const changeMenu = () => {
+        if (!navbar.classList.contains('menu-burguer-navbar')) {
+            navbar.classList.add('menu-burguer-navbar')
+            link[0].classList.add('menu-burguer-link')
+            link[1].classList.add('menu-burguer-link')
+            link[2].classList.add('menu-burguer-link')
+            menu.classList.add('display-none')
+            close.classList.remove('display-none')
+            document.documentElement.style.overflow = 'hidden'
+        } else {
+            navbar.classList.remove('menu-burguer-navbar')
+            link[0].classList.remove('menu-burguer-link')
+            link[1].classList.remove('menu-burguer-link')
+            link[2].classList.remove('menu-burguer-link')
+            menu.classList.remove('display-none')
+            close.classList.add('display-none')
+            document.documentElement.style.overflow = 'visible'
+        }
+    }
+    hamburguer.addEventListener('click', changeMenu)
 }
