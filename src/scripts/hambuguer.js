b7a1b01fa1e765050db0d1e31c88e5dbb5c6037d
PedroPiuma <lpap1000@gmail.com>
2022-04-13 18:49:15 -0300

    add menu mobile with JS, change CV, add contact form

--- src/scripts/hambuguer.js
+++ src/scripts/hambuguer.js
@@ -1,25 +1,31 @@
 const hamburguer = document.querySelector('.burguer-button')
-const navbar = document.querySelector('.navbar-links')
-const link = document.querySelectorAll('.navbar-link')
-const menu = document.querySelector('#menu')
-const close = document.querySelector('#close')
 const changeMenu = () => {
+    const navbar = document.querySelector('.navbar-links')
+    const link = document.querySelectorAll('.navbar-link')
+    const menu = document.querySelector('#menu')
+    const close = document.querySelector('#close')
+    navbar.classList.toggle('menu-burguer-navbar')
+    link.forEach(e => e.classList.toggle('menu-burguer-link'))
+    menu.classList.toggle('display-none')
+    close.classList.toggle('display-none')
+
     if (!navbar.classList.contains('menu-burguer-navbar')) {
-        navbar.classList.add('menu-burguer-navbar')
-        link[0].classList.add('menu-burguer-link')
-        link[1].classList.add('menu-burguer-link')
-        link[2].classList.add('menu-burguer-link')
-        menu.classList.add('display-none')
-        close.classList.remove('display-none')
-        document.documentElement.style.overflow = 'hidden'
-    } else {
-        navbar.classList.remove('menu-burguer-navbar')
-        link[0].classList.remove('menu-burguer-link')
-        link[1].classList.remove('menu-burguer-link')
-        link[2].classList.remove('menu-burguer-link')
-        menu.classList.remove('display-none')
-        close.classList.add('display-none')
         document.documentElement.style.overflow = 'visible'
+    } else {
+        document.documentElement.style.overflow = 'hidden'
     }
 }
 hamburguer.addEventListener('click', changeMenu)
+
+// link[0].classList.toggle('menu-burguer-link')
+// link[1].classList.toggle('menu-burguer-link')
+// link[2].classList.toggle('menu-burguer-link')
+// else {
+//     navbar.classList.remove('menu-burguer-navbar')
+//     link[0].classList.remove('menu-burguer-link')
+//     link[1].classList.remove('menu-burguer-link')
+//     link[2].classList.remove('menu-burguer-link')
+//     menu.classList.remove('display-none')
+//     close.classList.add('display-none')
+//     document.documentElement.style.overflow = 'visible'
+// }
